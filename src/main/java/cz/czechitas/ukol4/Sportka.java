d7ef82041ab2df0ddaca097287e3723a5a6eab99
Wanderthedog <jmichajlovicova@seznam.cz>
2024-03-03 16:27:49 +0100

    ukol 04

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -11,7 +11,10 @@ public class Sportka {
     private final List<Integer> osudi = new ArrayList<>();
 
     public Sportka() {
-        //TODO naplnit osudí čísly 1 až 49.
+        //naplnit osudí čísly 1 až 49.
+        for (int i = 1; i <= 49; i++) {
+            osudi.add(i);
+        }
     }
 
     /**
@@ -20,7 +23,8 @@ public class Sportka {
      * @see Collections#shuffle(List)
      */
     public void zamichej() {
-        //TODO zamíchat osudí
+        //zamíchat osudí
+        Collections.shuffle(osudi);
     }
 
     /**
@@ -30,8 +34,8 @@ public class Sportka {
      * @see List#subList(int, int)
      */
     public List<Integer> dejVylosovanaCisla() {
-        //TODO Vrátit seznam prvních 6 čísel z osudí.
-        return null;
+        //Vrátit seznam prvních 6 čísel z osudí.
+        return osudi.subList(0, 6);
     }
 
     /**
@@ -40,7 +44,6 @@ public class Sportka {
      * @return Dodatkové číslo.
      */
     public Integer dejDodatkoveCislo() {
-        //TODO Vrátit sedmé číslo z osudí.
-        return null;
+        return osudi.get(6);
     }
 }
